214043443d1111e36c10a89a66a2a01e16346892
zemscky <aleksei.zemscky@yandex.ru>
2022-09-30 17:30:24 +0300

    Домашняя работа 1. Задание 1. Методы.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-
     }
+    // Задание 1
+    public static void printIsLeapYear(int year) {
+        boolean leapYear = isLeapYear(year);
+        printIsLeapYearResult(year, leapYear);
+    }
+    private static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+    }
+    private static void printIsLeapYearResult(int year, boolean leapYear) {
+        if (leapYear) {
+            System.out.println(year + " — високосный год");
+        } else {
+            System.out.println(year + " — не високосный год");
+        }
+    }
+
 }
