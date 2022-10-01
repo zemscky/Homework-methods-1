85a7517ac7ff81ee951d1ffde645a56bf2252027
zemscky <aleksei.zemscky@yandex.ru>
2022-10-01 20:40:11 +0300

    Домашняя работа 1. Задание 1. Исправления

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int yearNumber = 2022;
+        printIsLeapYear(yearNumber);
+
+        System.out.println("Задание 2");
+        String osName = "iOS";
     }
+
     // Задание 1
     public static void printIsLeapYear(int year) {
         boolean leapYear = isLeapYear(year);
         printIsLeapYearResult(year, leapYear);
     }
+
     private static boolean isLeapYear(int year) {
         return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
     }
+
     private static void printIsLeapYearResult(int year, boolean leapYear) {
         if (leapYear) {
             System.out.println(year + " — високосный год");
