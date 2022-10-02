44ef431084d6dc8990339563ada9e01f5651364b
zemscky <aleksei.zemscky@yandex.ru>
2022-10-02 13:54:19 +0300

    Домашняя работа 1. Задание 3.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
-import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
         taskOne();
         taskTwo();
         taskThree();
     }
-    public static void taskOne () {
+
+    public static void taskOne() {
         System.out.println("Задание 1");
         int yearNumber = 2022;
         printIsLeapYear(yearNumber);
     }
+
     public static void printIsLeapYear(int year) {
         boolean leapYear = isLeapYear(year);
         printIsLeapYearResult(year, leapYear);
     }
+
     private static boolean isLeapYear(int year) {
         return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
     }
+
     private static void printIsLeapYearResult(int year, boolean leapYear) {
         if (leapYear) {
             System.out.println(year + " — високосный год");
@@ -26,17 +28,19 @@ public class Main {
         }
     }
 
-    public static void taskTwo () {
+    public static void taskTwo() {
         System.out.println("Задание 2");
         int deviceYear = 2016;
         String osName = "iOS";
-        printIsClientDevice (deviceYear, osName);
+        printIsClientDevice(deviceYear, osName);
     }
+
     public static void printIsClientDevice(int deviceYear, String osName) {
-        boolean clientDevice = clientDeviceYear (deviceYear);
-        int clientOS = getClientOS (osName);
-        printIsClientDeviceResult (deviceYear, clientDevice, osName, clientOS);
+        boolean clientDevice = clientDeviceYear(deviceYear);
+        int clientOS = getClientOS(osName);
+        printIsClientDeviceResult(deviceYear, clientDevice, osName, clientOS);
     }
+
     private static int getClientOS(String name) {
         if (name.equals("iOS")) {
             return 0;
@@ -44,13 +48,15 @@ public class Main {
             return 1;
         }
     }
-    private static boolean clientDeviceYear (int deviceYear) {
-       if (deviceYear > 2015) {
-           return true;
-       } else {
+
+    private static boolean clientDeviceYear(int deviceYear) {
+        if (deviceYear > 2015) {
+            return true;
+        } else {
             return false;
         }
     }
+
     private static void printIsClientDeviceResult(int deviceYear, boolean clientDevice, String osName, int clientOS) {
         if (clientDevice == true && clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
@@ -62,8 +68,18 @@ public class Main {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         }
     }
-    public static void taskThree () {
 
+    public static void taskThree() {
+        System.out.println("Задание 3");
+        int deliveryDistance = calculateDeliveryDays (20);
+        System.out.println("Потребуется дней: " + deliveryDistance);
     }
-
+    public static int calculateDeliveryDays (int deliveryDistance) {
+        if (deliveryDistance <= 20) {
+            return 1;
+        } else {
+            return (int) Math.round (deliveryDistance / 40) + 1;
+        }
+    }
 }
+
